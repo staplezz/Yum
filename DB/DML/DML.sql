205d1608654332e4d4cb7b0f3ed6f9a33078228c
Jorge Francisco Cortés López <kokofrank@ciencias.unam.mx>
2020-05-31 17:08:31 -0500

    Actualiza
    - Agrega ejemplos para todo el sistema

--- DB/DML/DML.sql
+++ DB/DML/DML.sql
@@ -1,15 +1,56 @@
 USE yum_db;
 
-INSERT INTO `yum_db`.`persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Alberto', 'Arándano', 'Álvarez', 'password', 'a@arandano.com');
-INSERT INTO `yum_db`.`persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Berta', 'Bolívar', 'Bolsa', 'password', 'b@bolivar.com');
-INSERT INTO `yum_db`.`persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Carla', 'Cortes', 'Contreras', 'password', 'c@caldo.com');
+/* DML para poblar la BDD con datos ficticios. */
 
-INSERT INTO `yum_db`.`administrador` (`idPersona`) VALUES ('1');
+/* DML personas */
+INSERT INTO `persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Alberto', 'ArÃ¡ndano', 'Álvarez', 'password', 'a@arandano.com');
+INSERT INTO `persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Berta', 'Bolívar', 'Bolsa', 'password', 'b@bolivar.com');
+INSERT INTO `persona` (`nombre`, `apellidoPaterno`, `apellidoMaterno`, `password`, `correoElectronico`) VALUES ('Carla', 'Cortes', 'Contreras', 'password', 'c@caldo.com');
 
-INSERT INTO `yum_db`.`categoria` (`nombre`) VALUES ('Botanas');
-INSERT INTO `yum_db`.`categoria` (`nombre`) VALUES ('Carnes');
+/* DML admin */
+INSERT INTO `administrador` (`idPersona`) VALUES ('1');
+/* DML cliente, su carrito y su dirección*/
+INSERT INTO `cliente` (`telefono`, `idPersona`) VALUES ('5565678960','2');
+INSERT INTO `carrito` (`idCliente`) VALUES ('1');
+INSERT INTO `direccion` (`delegacion`, `colonia`, `calle`, `num_interior`, `num_exterior`) VALUES ('Benito Juarez', 'Portales', 'Saratoga', '87', null);
+INSERT INTO `direccionescliente` (`idDireccion`, `idCliente`) VALUES ('1', '1');
+/* DML repartidor */
+INSERT INTO `repartidor` (`idPersona`) VALUES ('3');
 
-INSERT INTO `yum_db`.`alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Papas Fritas', '15', 'La papa perfecta para cuando tienes flojera', '1');
-INSERT INTO `yum_db`.`alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Chuleta', '100', '1 kilogramo de chuleta', '2');
-INSERT INTO `yum_db`.`alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Fritos', '7', 'Papa especial para acompañar con cerveza', '1');
-INSERT INTO `yum_db`.`alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Arrachera', '300', '1 kilogramo de carne de los dioses', '2');
+/* DML categorias */
+INSERT INTO `categoria` (`nombre`) VALUES ('Botanas');
+INSERT INTO `categoria` (`nombre`) VALUES ('Carnes');
+
+/* DML alimentos */
+INSERT INTO `alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Papas Fritas', '15', 'La papa perfecta para cuando tienes flojera', '1');
+INSERT INTO `alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Chuleta', '100', '1 kilogramo de chuleta', '2');
+INSERT INTO `alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Fritos', '7', 'Papa especial para acompañar con cerveza', '1');
+INSERT INTO `alimento` (`nombre`, `precio`, `descripcion`, `idCategoria`) VALUES ('Arrachera', '300', '1 kilogramo de carne de los dioses', '2');
+
+/* DML para crear una órden */
+/* Primero poblamos los alimentos del carrito. */
+INSERT INTO `alimentoscarrito` (`idCarrito`, `idAlimento`, `cantidad`) VALUES ('1', '1', '2');
+INSERT INTO `alimentoscarrito` (`idCarrito`, `idAlimento`, `cantidad`) VALUES ('1', '2', '1');
+
+/* Una vez que el cliente pida confirmar la órden, creamos un registro
+ * para la órden que queremos almacenar del cliente */
+INSERT INTO `ordenescliente` (`idCliente`) VALUES ('1');
+
+/* Luego copiamos los alimentos del carrito
+* para que se almacenen usando la llave que recibimos en la inserción anterior.
+*/
+INSERT INTO `alimentosorden` (`idOrdenesCliente`, `idAlimento`, `cantidad`)
+SELECT /*Aquí iría la llave ->*/ '1', `idAlimento`, `cantidad`
+FROM `alimentoscarrito` WHERE `idCarrito` = '1';
+
+/* Una vez guardados los alimentos, idealemnente tendríamos que borrar los alimentos del carrito 
+* DELETE FROM `alimentoscarrito` WHERE `idCarrito` = '1';
+*/
+
+/* Finalmente ya podemos generar la órden en la BDD usando la llave que usamos en la tabla alimentosorden
+* para así llevar registro de las órdenes de cada cliente.
+* (Nota: al incio una orden tendrá el repartidor vacío, porque aún nadie toma la órden hasta que el estado
+* sea: listo para entregar) 
+*/
+INSERT INTO `orden` (`fecha`, `estado`, `calificacion`, `idCliente`, `idOrdenesCliente`, `idRepartidor`)
+VALUES ('2020-05-31', '1', null, '1', '1', null);
